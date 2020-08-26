c653ab24bdc7ce4771ddfac8f45f64993b95b037
Edward Smith <edward.m.smith80@gmail.com>
2020-08-26 21:09:41 +0000

    client and play are set up to run

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+// this establishes connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '10.0.2.15', // am i supposed to use this or localhost
+    port: 50541
+  });
+
+  // this lets us interpret incomeing data from the server as text
+  conn.setEncoding('utf8');
+  // conn will now use the built-in 'on' method to handle incoming data
+  // 
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  return conn;
+}
+
+module.exports = { connect }
